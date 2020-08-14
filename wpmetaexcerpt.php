1c29547f56bcf58c3a4bcb1d2bf34a35d402cb12
jefechang <carnevali.filippo@gmail.com>
2020-08-14 13:23:02 +0200

    Create wpmetaexcerpt.php

---
+++ wpmetaexcerpt.php
@@ -0,0 +1,18 @@
+
+<?php
+/**
+ * Plugin Name: Wordpress Excerpt to Meta description
+ * Description: Snippet per aggiungere dinamicamente l'excerpt degli articoli alla description e avere quindi controllo sul testo che facebook visualizza subito dopo l'articolo. Testato con developers.facebook.com/tools/debug/
+ * Plugin URI:  https://github.com/jefechang
+ * Version:     1.0.0
+ * Author:      JefeChang
+ * Author URI:  https://github.com/jefechang
+ * Text Domain: effecimetaexcerpt
+ */
+
+function effeci_metaexcerpt () {
+if ( is_single() ) {
+?><meta name="description" content="<?php echo wp_strip_all_tags( get_the_excerpt(), true ); ?>" />
+<?php } 
+}
+add_action( 'wp_head', 'effeci_metaexcerpt', 10 );
